afb277145834ccc470d882d0cc09809f94beef86
Ikennaf1 <iiikbros202@gmail.com>
2023-03-01 21:54:15 +0000

    3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,61 +10,64 @@ void binary_tree_delete(binary_tree_t *tree)
     if (tree == NULL)
         return;
 
-    if (tree->left == NULL && tree->right == NULL) {
+    if (tree->left == NULL && tree->right == NULL)
         return;
-    } else {
 
-    // if (tree->left != NULL) {
-    //     tree = tree->left;
-    //     binary_tree_delete(tree);
-    //     free(tree);
-    //     if (tree->parent != NULL) {
-    //         free(tree->parent);
-    //         tree->parent = NULL;
-    //     }
-    //     tree = NULL;
-    //     return;
-    // }
-
-    // if (tree->right != NULL) {
-    //     tree = tree->right;
-    //     binary_tree_delete(tree);
-    //     free(tree);
-    //     if (tree->parent != NULL) {
-    //         free(tree->parent);
-    //         tree->parent = NULL;
-    //     }
-    //     tree = NULL;
-    //     return;
-    // }
-
-    // free(tree);
-    // tree = NULL;
-    // return;
-
-        if (tree->left) {
-            tree = tree->left;
-            binary_tree_delete(tree);
-            return;
-        }
-        if (tree->right) {
-            tree = tree->right;
-            binary_tree_delete(tree);
-            return;
-        }
-        
-        if (tree->left)
-            free(tree->left);
-
-        if (tree->right)
-            free(tree->right);
-
-        if (tree->parent)
-            free(tree->parent);
-
-        // if (tree)
+    if (tree->left != NULL) {
+        tree = tree->left;
+        binary_tree_delete(tree);
         // free(tree);
-        
+        // if (tree->parent != NULL) {
+        //     free(tree->parent);
+        //     tree->parent = NULL;
+        // }
+        // tree = NULL;
         return;
     }
+
+    if (tree->right != NULL) {
+        tree = tree->right;
+        binary_tree_delete(tree);
+        // free(tree);
+        // if (tree->parent != NULL) {
+        //     free(tree->parent);
+        //     tree->parent = NULL;
+        // }
+        // tree = NULL;
+        return;
+    }
+
+    if (tree->left) {
+        free(tree->left);
+        tree->left = NULL;
+    }
+    if (tree->parent->left) {
+        free(tree->parent->left);
+        tree->parent->left = NULL;
+    }
+    
+    if (tree->right) {
+        free(tree->right);
+        tree->right = NULL;
+    }
+    if (tree->parent->right) {
+        free(tree->parent->right);
+        tree->parent->right = NULL;
+    }
+    
+    if (tree->parent) {
+        free(tree->parent);
+        tree->parent = NULL;
+    }
+    if (tree->parent->parent) {
+        free(tree->parent->parent);
+        tree->parent->parent = NULL;
+    }
+    
+    if (tree) {
+        free(tree);
+        tree = NULL;
+    }
+
+    return;
 }
